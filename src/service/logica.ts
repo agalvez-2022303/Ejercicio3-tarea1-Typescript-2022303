3100f8cc2799d4b17850d797690f43b0cfac4e52
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-06-12 22:25:08 -0600

    feat: implementar servicio crearIncidente

---
+++ src/service/logica.ts
@@ -0,0 +1,24 @@
+﻿import { Incident } from '../model/Incident';
+import { CrearIncidente } from '../types/tipos';
+
+let ultimoID = 0;
+
+export const incidentes: Incident[] = [];
+
+export function crearIncidente(datos: CrearIncidente): Incident {
+    ultimoID = ultimoID + 1;
+
+    const nuevoIncidente: Incident = {
+        id: ultimoID,
+        titulo: datos.titulo,
+        descripcion: datos.descripcion,
+        reportadoPor: datos.reportadoPor,
+        prioridad: datos.prioridad,
+        estado: datos.estado,
+        fechaCreacion: new Date()
+    };
+
+    incidentes.push(nuevoIncidente);
+
+    return nuevoIncidente;
+}
