e3edd4f91483655f9b1e1bd1f73f53baf070b82d
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-06-12 22:25:08 -0600

    feat: agregar modelo Incident y tipos base

---
+++ src/model/Incident.ts
@@ -0,0 +1,10 @@
+﻿import { Prioridad, estadoIncidente } from '../types/tipos';
+export interface Incident { 
+    readonly id: number; 
+    titulo: string;
+    descripcion: string; 
+    reportadoPor: string;
+    prioridad: Prioridad;
+    estado: estadoIncidente;
+    fechaCreacion: Date;
+} 
---
+++ src/types/tipos.ts
@@ -0,0 +1,4 @@
+﻿import { Incident } from '../model/Incident';
+export type Prioridad = 'alta' | 'media' | 'baja';
+export type estadoIncidente = 'abierto' | 'progreso' | 'resuelto';
+export type CrearIncidente = Omit<Incident, 'id' | 'fechaCreacion'>;
